8558d61813c36b7c2e6a2b2499dc48a4475c0868
Marques <marquesdutra1@gmail.com>
2022-04-26 19:18:06 -0300

    Python

---
+++ pythonProject/ex0001.py
@@ -0,0 +1,11 @@
+print('ola mundo')
+
+
+def floot(param):
+    pass
+
+print = input('quer calcular que tipo de número?[inteiros/reais]'))
+n1 = float(input('priemiro número:'))
+n2 = float(input('segundo número:'))
+r = n1 + n2
+print('a soma é ', r)
